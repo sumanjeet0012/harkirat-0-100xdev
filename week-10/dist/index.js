74335a5dc977c18cf8fe1dc701a0090536a3372c
Sumanjeet <sumanjeet0012@gmail.com>
2024-05-28 01:58:48 +0530

    joins in a secure form

--- week-10/dist/index.js
+++ week-10/dist/index.js
@@ -122,4 +122,33 @@ function getUserAndAddress() {
         }
     });
 }
-getUserAndAddress();
+function getUserDetailsWithAddress(userId) {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            yield client.connect();
+            const query = `
+            SELECT u.id, u.username, u.email, a.city, a.country, a.street, a.pincode
+            FROM users u
+            JOIN addresses a ON u.id = a.user_id
+            WHERE u.id = $1
+        `;
+            const result = yield client.query(query, [userId]);
+            if (result.rows.length > 0) {
+                console.log('User and address found:', result.rows[0]);
+                return result.rows[0];
+            }
+            else {
+                console.log('No user or address found with the given ID.');
+                return null;
+            }
+        }
+        catch (err) {
+            console.error('Error during fetching user and address:', err);
+            throw err;
+        }
+        finally {
+            yield client.end();
+        }
+    });
+}
+getUserDetailsWithAddress("1");
--- week-10/src/index.ts
+++ week-10/src/index.ts
@@ -103,4 +103,31 @@ async function getUserAndAddress() {
     }
 }
 
-getUserAndAddress();
+async function getUserDetailsWithAddress(userId: string) {
+
+    try {
+        await client.connect();
+        const query = `
+            SELECT u.id, u.username, u.email, a.city, a.country, a.street, a.pincode
+            FROM users u
+            JOIN addresses a ON u.id = a.user_id
+            WHERE u.id = $1
+        `;
+        const result = await client.query(query, [userId]);
+
+        if (result.rows.length > 0) {
+            console.log('User and address found:', result.rows[0]);
+            return result.rows[0];
+        } else {
+            console.log('No user or address found with the given ID.');
+            return null;
+        }
+    } catch (err) {
+        console.error('Error during fetching user and address:', err);
+        throw err;
+    } finally {
+        await client.end();
+    }
+}
+
+getUserDetailsWithAddress("1");
