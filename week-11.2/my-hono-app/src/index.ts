172c9b421de1456728fa70fe33f6795d7dcb87ff
Sumanjeet <sumanjeet0012@gmail.com>
2024-05-29 17:14:18 +0530

    auth middleware in hono

--- week-11.2/my-hono-app/src/index.ts
+++ week-11.2/my-hono-app/src/index.ts
@@ -3,7 +3,18 @@ const app = new Hono()
 
 // app.get('/', (c) => c.text('Hello Cloudflare Workers!'))
 
-app.post('/', async (c) => {
+async function authMiddleware (c: any, next: any) {
+  if (c.req.header("Authorization")) {
+    // Do validation
+    await next()
+  } else {
+    return c.text("You dont have acces");
+  }
+}
+
+// app.use(authMiddleware);
+
+app.post('/', authMiddleware,async (c) => {
   const body = await c.req.json();
   console.log(body);
   console.log(c.req.header("Authorization"));
