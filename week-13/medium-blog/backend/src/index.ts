72f5859259e8ddbb40d17924d0f46f837b439719
Sumanjeet <sumanjeet0012@gmail.com>
2024-06-13 20:50:29 +0530

    build some basic routes

--- week-13/medium-blog/backend/src/index.ts
+++ week-13/medium-blog/backend/src/index.ts
@@ -6,4 +6,28 @@ app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
 
+app.post('/api/v1/user/signup', (c) => {
+  return c.text('This is a signup route')
+})
+
+app.post('/api/v1/user/signin', (c) => {
+  return c.text('This is a signin route')
+})
+
+app.post('/api/v1/blog', (c) => {
+  return c.text('This is a blog route')
+})
+
+app.put('/api/v1/blog', (c) => {
+  return c.text('This is a blog route')
+})
+
+app.get('/api/v1/blog/:id', (c) => {
+  return c.text('This is a blog route with id')
+})
+
+app.get('/api/v1/blog/bulk', (c) => {
+  return c.text('This is a blog route with bulk')
+})
+
 export default app
