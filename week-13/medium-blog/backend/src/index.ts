593d392d54a225b201f706a0d1607ce2212c5459
Sumanjeet <sumanjeet0012@gmail.com>
2024-06-15 23:27:51 +0530

    using env in hono

--- week-13/medium-blog/backend/src/index.ts
+++ week-13/medium-blog/backend/src/index.ts
@@ -1,33 +1,42 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
 
-const app = new Hono()
+const app = new Hono<{
+	Bindings: {
+		DATABASE_URL: string
+	}
+}>();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get("/", (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+  return c.text("Hello Hono!");
+});
 
-app.post('/api/v1/user/signup', (c) => {
-  return c.text('This is a signup route')
-})
+app.post("/api/v1/user/signup", (c) => {
+  return c.text("This is a signup route");
+});
 
-app.post('/api/v1/user/signin', (c) => {
-  return c.text('This is a signin route')
-})
+app.post("/api/v1/user/signin", (c) => {
+  return c.text("This is a signin route");
+});
 
-app.post('/api/v1/blog', (c) => {
-  return c.text('This is a blog route')
-})
+app.post("/api/v1/blog", (c) => {
+  return c.text("This is a blog route");
+});
 
-app.put('/api/v1/blog', (c) => {
-  return c.text('This is a blog route')
-})
+app.put("/api/v1/blog", (c) => {
+  return c.text("This is a blog route");
+});
 
-app.get('/api/v1/blog/:id', (c) => {
-  return c.text('This is a blog route with id')
-})
+app.get("/api/v1/blog/:id", (c) => {
+  return c.text("This is a blog route with id");
+});
 
-app.get('/api/v1/blog/bulk', (c) => {
-  return c.text('This is a blog route with bulk')
-})
+app.get("/api/v1/blog/bulk", (c) => {
+  return c.text("This is a blog route with bulk");
+});
 
-export default app
+export default app;
