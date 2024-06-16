8ec66a836fdd97bed15e17090122902c2c315f2f
Sumanjeet <sumanjeet0012@gmail.com>
2024-06-16 20:27:20 +0530

    authentication middleware for blogs route is created

--- week-13/medium-blog/backend/src/index.ts
+++ week-13/medium-blog/backend/src/index.ts
@@ -1,7 +1,7 @@
 import { Hono } from "hono";
 import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
-import { jwt, sign } from "hono/jwt";
+import { jwt, sign, verify } from "hono/jwt";
 
 const app = new Hono<{
   Bindings: {
@@ -9,6 +9,29 @@ const app = new Hono<{
     JWT_SECRET: string;
   };
 }>();
+
+app.use("/api/v1/blog/*", async (c, next) => {
+  const header = c.req.header("Authorization");
+  const token = header?.split(" ")[1];
+  if (!token) {
+    return c.json({ error: "Unauthorized" });
+  }
+  try {
+    const verified = await verify(token, c.env.JWT_SECRET);
+    if(verified.id){
+      await next();
+    } else {
+      c.status(403);
+      return c.json({"error": "JWT verification failed"});
+    }
+  } catch (error) {
+    c.json({"error": "JWT verification failed"});
+    c.text("JWT verification failed");
+    return c.status(403);
+  }
+  
+})
+
 
 /*
   `//@ts-ignore` it is used to ignore type check on next line.
