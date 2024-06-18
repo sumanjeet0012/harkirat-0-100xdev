172365682977e62bab84017a1ad916826cbfd94e
Sumanjeet <sumanjeet0012@gmail.com>
2024-06-18 11:13:05 +0530

    Quote component created

---
+++ week-13/medium-blog/frontend/src/components/Quote.tsx
@@ -0,0 +1,21 @@
+const Quote = () => {
+  return (
+    <div className="bg-gray-300 h-screen flex justify-center flex-col">
+      <div className="flex justify-center">
+        <div className="max-w-lg">
+            <div className="text-3xl font-bold">
+                "The customer support I received was exceptional. The support team went above and beyond to address my concerns"
+            </div>
+            <div className="max-w-md text-xl font-semibold text-left mt-4">
+                Jules WinnField
+            </div>
+            <div className="max-w-md text-sm font-semibold text-left">
+                CEO | Sumanjeet    
+            </div>          
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Quote;
