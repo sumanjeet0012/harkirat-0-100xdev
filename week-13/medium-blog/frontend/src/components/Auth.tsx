39b2a22b62f55fe6fc975ccaac865d190ed31146
Sumanjeet <sumanjeet0012@gmail.com>
2024-06-18 17:40:11 +0530

    auth page design complete

---
+++ week-13/medium-blog/frontend/src/components/Auth.tsx
@@ -0,0 +1,88 @@
+import { SignupInput } from "@sumanjeet0012/medium-common";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+const Auth = ({ type }: { type: "signin" | "signup" }) => {
+  const [postInputs, setPostInputs] = useState<SignupInput>({
+    name: "",
+    username: "",
+    password: "",
+  });
+
+  return (
+    <div className="h-screen flex justify-center flex-col">
+      <div className="flex justify-center">
+        <div className="w-7/12">
+          <div className="text-3xl font-extrabold">Create an account</div>
+          <div>
+            Already have an account? 
+            <Link className="pl-2 underline" to="/signin">
+              Login
+            </Link>
+          </div>
+          <div className="pt-8">
+            <LabelledInput
+              label="Name"
+              placeholder="Enter your Name"
+              onChange={(e) =>
+                setPostInputs({ ...postInputs, name: e.target.value })
+              }
+            />
+            <LabelledInput
+              label="Username"
+              placeholder="Enter your Username"
+              onChange={(e) =>
+                setPostInputs({ ...postInputs, username: e.target.value })
+              }
+            />
+            <LabelledInput
+              label="Password"
+              placeholder="Enter your Password"
+              type="password"
+              onChange={(e) =>
+                setPostInputs({ ...postInputs, password: e.target.value })
+              }
+            />
+            <button
+              type="button"
+              className="w-full mt-4 text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
+            >
+              Submit
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Auth;
+
+interface LabelledInputType {
+  label: string;
+  placeholder: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  type?: string;
+}
+
+function LabelledInput({
+  label,
+  placeholder,
+  onChange,
+  type,
+}: LabelledInputType) {
+  return (
+    <div>
+      <label className="block mb-2 text-sm font-medium text-black pt-2">
+        {label}
+      </label>
+      <input
+        onChange={onChange}
+        type={type || "text"}
+        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
+        placeholder={placeholder}
+        required
+      />
+    </div>
+  );
+}
