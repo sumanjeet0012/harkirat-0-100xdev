5288faaa5bec985bb67e84fca316f867beb1eb09
Sumanjeet <sumanjeet0012@gmail.com>
2024-06-20 00:27:34 +0530

    publish page created

--- week-13/medium-blog/frontend/src/components/Appbar.tsx
+++ week-13/medium-blog/frontend/src/components/Appbar.tsx
@@ -8,6 +8,15 @@ export const Appbar = () => {
         <Link to={"/"}>Medium</Link>
       </div>
       <div>
+        <Link to={"/publish"}>
+          <button
+            type="button"
+            className=" mr-8 text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+          >
+            New
+          </button>
+        </Link>
+
         <Avatar name="Sumanjeet" size="large" />
       </div>
     </div>
