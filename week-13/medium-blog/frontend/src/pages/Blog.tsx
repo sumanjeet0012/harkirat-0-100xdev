6f424b75fd0c9369bc515aa8769371ca30b2ab94
Sumanjeet <sumanjeet0012@gmail.com>
2024-06-19 16:58:42 +0530

    single blog page created

--- week-13/medium-blog/frontend/src/pages/Blog.tsx
+++ week-13/medium-blog/frontend/src/pages/Blog.tsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import { useBlog } from "../hooks";
+import { useParams } from "react-router-dom";
+import { Appbar } from "../components/Appbar";
+import { FullBlog } from "../components/FullBlog";
 
 export const Blog = () => {
+  const { id } = useParams();
+  const { loading, blog } = useBlog(Number(id));
+
+  if (loading || !blog) {
+    return (
+      <div>
+        <Appbar />
+
+        <div className="h-screen flex flex-col justify-center">
+          <div className="flex justify-center">Loading ...</div>
+        </div>
+      </div>
+    );
+  }
   return (
-    <div>Blog</div>
-  )
-}
+    <div>
+      <FullBlog blog={blog} />
+    </div>
+  );
+};
