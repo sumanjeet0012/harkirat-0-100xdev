8d1c6f599a02cd9e4f1d5a0c402b22241e30ce95
Sumanjeet <sumanjeet0012@gmail.com>
2024-06-19 16:57:56 +0530

    useBlog custom hook created to get single blog

--- week-13/medium-blog/frontend/src/hooks/index.ts
+++ week-13/medium-blog/frontend/src/hooks/index.ts
@@ -11,6 +11,24 @@ export interface Blog {
     }
 }
 
+export const useBlog = (id: number) => {
+    const [loading , setLoading] = useState(true);
+    const [blog, setBlog] = useState<Blog>();
+
+    useEffect(() => {
+        axios.get(`${BACKEND_URL}/api/v1/blog/${id}`, {
+            headers: {
+                Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTB9.2EiH7Zxs0v4r0LLFFBtb1FL4Ag6fnmy1BNM8yAA_e2o`
+            }
+        }).then((res) => {
+            setBlog(res.data.blog);
+            setLoading(false);
+        })
+    }, [])
+
+    return { loading, blog };
+}
+
 export const useBlogs = () => {
     const [loading , setLoading] = useState(true);
     const [blogs, setBlogs] = useState<Blog[]>([]);
