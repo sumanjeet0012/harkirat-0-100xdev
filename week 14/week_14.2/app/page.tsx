4b50a9b38df4140e301997651844d6c15dab1f78
Sumanjeet <sumanjeet0012@gmail.com>
2024-06-21 09:40:31 +0530

    data fetching and server side rendering

--- week 14/week_14.2/app/page.tsx
+++ week 14/week_14.2/app/page.tsx
@@ -1,9 +1,24 @@
+import axios from "axios";
 
+async function getUserDetails() {
+  const response = await axios.get("https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details")
+	return response.data;
+}
 
-export default function Home() {
+export default async function Home() {
+  const res = await getUserDetails()
   return (
-    <div>
-      Hi There
+    <div className="flex flex-col justify-center h-screen">
+        <div className="flex justify-center">
+            <div className="border p-8 rounded">
+                <div>
+                    Name: {res?.name}
+                </div>
+                
+                {res?.email}
+            </div>
+        </div>
     </div>
+    
   );
 }
