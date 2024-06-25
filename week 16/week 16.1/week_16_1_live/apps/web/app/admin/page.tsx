057b01dd78e6c3fe4e1b3b2f305d0aa4270dbf7d
Sumanjeet <sumanjeet0012@gmail.com>
2024-06-25 08:46:39 +0530

    react app added

---
+++ week 16/week 16.1/week_16_1_live/apps/web/app/admin/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import {Admin} from '@repo/ui/admin'
+
+const page = () => {
+  return (
+    <div>admin page
+        <div>
+            <Admin />
+        </div>
+
+    </div>
+  )
+}
+
+export default page
