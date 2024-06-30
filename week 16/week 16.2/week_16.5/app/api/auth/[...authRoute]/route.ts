9c89cd664a019b0641001ca94c365a35d2dcaad0
Sumanjeet <sumanjeet0012@gmail.com>
2024-06-30 06:17:18 +0530

    use of params in next js

--- week 16/week 16.2/week_16.5/app/api/auth/[...authRoute]/route.ts
+++ week 16/week 16.2/week_16.5/app/api/auth/[...authRoute]/route.ts
@@ -1,9 +1,16 @@
-import { NextResponse } from "next/server"
+import { NextRequest, NextResponse } from "next/server"
 
 // it will work ffor /api/auth/* route
 
 export function GET () {
     return NextResponse.json({
         success: true
+    })
+}
+
+export function POST (req : NextRequest, {params} : any) {
+    return NextResponse.json({
+        success: true,
+        params
     })
 }
