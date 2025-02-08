45715ad6a569c9a1cd94657c24184259893986a9
Sumanjeet <Sumanjeet0012@gmail.com>
2025-02-08 13:33:39 +0530

    react frontend for websockets built

--- week 19/websockets/react-frontend/frontend/src/App.tsx
+++ week 19/websockets/react-frontend/frontend/src/App.tsx
@@ -1,15 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [socket, setSocket] = useState<null | WebSocket>(null)
+  const [latestMessage, setLatestMessage] = useState("")
+  const [messages, setMessages] = useState("")
+
+  useEffect(() => {
+    const socket = new WebSocket("ws://localhost:8080")
+    socket.onopen = () => {
+      console.log("Connected")
+      setSocket(socket)
+    }
+    socket.onmessage = (message) => {
+      console.log('Received message:', message.data)
+      setLatestMessage(message.data)
+    }
+    socket.onclose = () => {
+      console.log("Disconnected")
+    }
+    return () => {
+      socket.close()
+    }
+  }, [])
+
+  if (!socket) {
+    return (
+      <>
+        <p>Loading...</p>
+      </>
+    )
+  }
 
   return (
-    <>
-      <p>Hello World!</p>
-    </>
+<div>
+
+<input type="text" onChange= {e => setMessages(e.target.value)} />
+<button onClick={() => socket.send(messages)}>Send</button>
+
+{latestMessage}
+</div>
   )
 }
 
